6e030799b929305c441d075a48fb408c233bc736
reuschelcm <54403617+reuschelcm@users.noreply.github.com>
2019-09-09 13:08:32 -0700

    Update myList.sh

--- bin/myList.sh
+++ bin/myList.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 # create a file to write to
-# DIRECTORY_FILE="directory.html"
+DIRECTORY_FILE="directory.html"
+
+echo "<!DOCTYPE html>" >> $DIRECTORY_FILE
+echo "<html>" >> $DIRECTORY_FILE
+echo "<body>" >> $DIRECTORY_FILE
+
 
 # create an array with all the filer/dir inside ~/myDir
 arr=$( ls )
 
 # iterate through the array to cycle through files
 for ((i=0; i<${#arr[@]}; i++)); do
-    echo "${arr[$i]}" # > DIRECTORY_FILE
-done
+    echo "${arr[$i]}" >> $DIRECTORY_FILE
+    echo "<br>" >> $DIRECTORY_FILE
+done 
+
+echo "</body>" >> $DIRECTORY_FILE
+echo "</html>" >> $DIRECTORY_FILE
