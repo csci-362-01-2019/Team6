abbb22204fed3d3d81e9f87eb7a61923370b52d8
reuschelcm <54403617+reuschelcm@users.noreply.github.com>
2019-09-09 15:58:31 -0700

    Update myList.sh

--- bin/myList.sh
+++ bin/myList.sh
@@ -9,9 +9,15 @@ echo "<p>"
 
 # iterate through the array of ls words 
 for i in $( ls ); do
-    echo $i 
-    echo "<br>"
-done 
+    if [[ $i != "toplevel.html" ]];
+	then
+    	    echo $i 
+    	    echo "<br>"
+    
+    fi;
+    #echo $i 
+    #echo "<br>"	
+done
 
 echo "</p>"
 echo "</body>"
