7e4ee936b918357863304e0a55d3389a67ac9a6e
Alan Arsiniega <arsiniegaa@g.cofc.edu>
2019-09-09 22:01:47 -0400

    Cleaned some comments, now ignoringthe .html document from top level files and folders

--- bin/myList.sh
+++ bin/myList.sh
@@ -9,14 +9,12 @@ echo "<p>"
 
 # iterate through the array of ls words 
 for i in $( ls ); do
-    if [[ $i != "toplevel.html" ]];
+    if [ $i != "toplevel.html" ];
 	then
     	    echo $i 
     	    echo "<br>"
     
     fi;
-    #echo $i 
-    #echo "<br>"	
 done
 
 echo "</p>"
