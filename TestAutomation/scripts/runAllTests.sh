ceaa588775b50100cb6a5c149f7dbf9ca44e6698
reuschelcm <54403617+reuschelcm@users.noreply.github.com>
2019-10-17 06:11:54 -0700

    Updated test script
    Now can loop through all files in the test directory

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -5,6 +5,7 @@
 ## ./runAllTests.sh
 
 #input variables
+InputFile=""
 TestID=""
 Requirement=""
 Component=""
@@ -24,89 +25,106 @@ declare -i x=1
 #compile OpenMRS dependencies
 #TODO: pom script
 
+#remove temp files
+cd ..
+rm -rf temp
+mkdir temp
+
+#begin report
+#TODO: go to report directory and begin
+cd "reports"
+echo "Test Report" > "testReport.html"
+
 #go to test file directory
 cd ..
 cd "testCases"
 
-#take in file
-InputFile="testCase0.txt" #TODO: Loop through all test cases
+#loop through all files
+caseFiles=$( ls * )
+for i in $caseFiles ; do
 
-#read file
-while IFS= read -r line
-do
-	#case statement to take file input
-	case $x in
-		#take in TestID
-		1)
-			TestID="$line"
-			;;
-		2)
-			Requirement="$line"
-			;;
-		3)
-			Component="$line"
-			;;
-		4)
-			Method="$line"
-			;;
-		5)
-			TestFile="$line"
-			;;
-		6)
-			Inputs="$line"
-			;;
-		*)
-			echo "Error"
-			;;
-	esac
+	#take in file
+	InputFile="$i"
 
-	#iterate counter variable
-	let x=$x+1
+	#read file
+	while IFS= read -r line
+	do
+		#case statement to take file input
+		case $x in
+			#take in TestID
+			1)
+				TestID="$line"
+				;;
+			2)
+				Requirement="$line"
+				;;
+			3)
+				Component="$line"
+				;;
+			4)
+				Method="$line"
+				;;
+			5)
+				TestFile="$line"
+				;;
+			6)
+				Inputs="$line"
+				;;
+			*)
+				echo "Error"
+				;;
+		esac
 
-done < "$InputFile"
+		#iterate counter variable
+		let x=$x+1
 
-#!echo test
-echo "Input File Read:"
-echo $TestID
-echo $Requirement
-echo $Component
-echo $Method
-echo $TestFile
-echo $Inputs
+	done < "$i"
 
-#format needed files
-JavaFile="${TestFile}.java"
-OutputFile="${TestID}results.txt"
-Oracle="${TestID}Oracle.txt"
+	#!echo test
+	echo "Input File Read:"
+	echo $TestID
+	echo $Requirement
+	echo $Component
+	echo $Method
+	echo $TestFile
+	echo $Inputs
 
-#go to executable directory
-cd ..
-cd "testCasesExecutables"
+	#format needed files
+	JavaFile="${TestFile}.java"
+	OutputFile="${TestID}results.txt"
+	Oracle="${TestID}Oracle.txt"
 
-#compile and execute
-javac $JavaFile
-java $TestFile $Inputs
+	#go to executable directory
+	cd ..
+	cd "testCaseExecutables"
 
-#go to home directory
-cd ..
+	#compile and execute
+	javac $JavaFile
+	java $TestFile $Inputs
 
-#compare output to oracle
-if cmp -s "temp/${OutputFile}" "oracles/${Oracle}"; then
-	TestStatus="Success"
-else
-	TestStatus="Failure"
-fi
-echo $TestStatus
+	#go to home directory
+	cd ..
 
-#go to report directory
-cd "reports"
+	#compare output to oracle
+	if cmp -s "temp/${OutputFile}" "oracles/${Oracle}"; then
+		TestStatus="Success"
+	else
+		TestStatus="Failure"
+	fi
+	echo $TestStatus
 
-#create/write to report
-echo "${TestID} Result: ${TestStatus}" >> "testReport.txt"
+	#go to report directory
+	cd "reports"
+
+	#create/write to report
+	echo "${TestID} Result: ${TestStatus}" >> "testReport.txt"
+
+	# return to test case directory for next test
+	cd ..
+	cd "testCases"
+done
+
+## TODO: display html report to browser
 
 #TODO:
-## Iterate through multiple files
-## Compile needed OpenMRS packages
-## clear temp folder before run
-## remove or rename old report file before another run
 ## {Driver File} write to temp
