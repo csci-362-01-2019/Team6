edef1e16257fca650d964cc04f5248c4e0c531dc
arsiniega <arsiniegaa@g.cofc.edu>
2019-10-24 16:40:15 -0400

    Added args input to test driver

--- TestAutomation/openmrs-core/api/src/main/java/org/openmrs/util/DoubleRangeTest.java
+++ TestAutomation/openmrs-core/api/src/main/java/org/openmrs/util/DoubleRangeTest.java
@@ -3,15 +3,11 @@ package org.openmrs.util;
 public class DoubleRangeTest {
 	
 	public static void main(String[] args) {
+		Double d1 = Double.parseDouble(args[0]);
+		Double d2 = Double.parseDouble(args[1]);
+		Double d3 = Double.parseDouble(args[2]);
 		
-		DoubleRange dr = new DoubleRange();
-		
-		// should be null
-		System.out.println(dr.getHigh());
-		
-		dr.setLow(new Double(7.8));
-		
-		// should be 7.8
-		System.out.println(dr.getLow());
+		DoubleRange dr = new DoubleRange(d1, d2);
+		System.out.println(dr.contains(d3));
 	}
 }
