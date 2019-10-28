63e83959c6fdc197aa9d7f735dfcdb8961480de4
JBallou-1 <54325784+JBallou-1@users.noreply.github.com>
2019-10-27 20:04:38 -0400

    Create HttpUrlTestJacob.java
    temporary version

---
+++ TestAutomation/openmrs-core/api/src/main/java/org/openmrs/util/HttpUrlTestJacob.java
@@ -0,0 +1,11 @@
+package org.openmrs.util;
+
+public class HttpUrlTest {
+	
+	public static void main(String arg) {
+  String str = new String;
+		HttpUrl url = new HttpUrl(arg);
+    #catch exception here
+		System.out.println(str)
+	}
+}
