977051075b4fafb1f50c66e5b9430451fa7ba4c7
Alan Arsiniega <arsiniegaa@cofc.edu>
2019-11-11 16:01:37 -0500

    Renamed to better illustrate method being tested

--- TestAutomation/testCaseExecutables/DoubleRangeContainsTest.java
+++ TestAutomation/testCaseExecutables/DoubleRangeContainsTest.java
@@ -2,11 +2,14 @@ package org.openmrs.util;
 
 import org.openmrs.util.DoubleRange;
 
-public class DoubleRangeTest {
+public class DoubleRangeContainsTest {
 	
 	public static void main(String[] args) {
 		Double d1 = Double.parseDouble(args[0]);
 		Double d2 = Double.parseDouble(args[1]);
+		
+		DoubleRange dr = new DoubleRange(d1, d2);
+		
 		Double d3 = Double.parseDouble(args[2]);
 		
 		DoubleRange dr = new DoubleRange(d1, d2);
