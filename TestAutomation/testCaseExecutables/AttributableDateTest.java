28733fead12d53de3843ad3cd8cdcbbdb069b919
Alan Arsiniega <arsiniegaa@cofc.edu>
2019-11-10 21:43:43 -0500

    Added two more java classes to test

---
+++ TestAutomation/testCaseExecutables/AttributableDateTest.java
@@ -0,0 +1,12 @@
+package org.openmrs.util;
+
+import org.openmrs.util.AttributableDate;
+
+// from /Team6/TestAutomation/testCaseExecutables
+// javac -cp ../openmrs-core/api/target/classes -d ../openmrs-core/api/target/classes/ ResultTest.java
+// java -cp ../openmrs-core/api/target/classes/ org.openmrs.logic.result.ResultTest
+public class AttributableDateTest {
+	public static void main(String[] args) {
+        
+	}
+}
---
+++ TestAutomation/testCaseExecutables/ResultTest.java
@@ -0,0 +1,22 @@
+package org.openmrs.logic.result;
+
+import org.openmrs.logic.result.Result;
+
+//from /Team6/TestAutomation/testCaseExecutables
+//javac -cp ../openmrs-core/api/target/classes -d ../openmrs-core/api/target/classes/ AttributableDateTest.java
+//java -cp ../openmrs-core/api/target/classes/ org.openmrs.util.AttributableDateTest
+public class ResultTest {
+	
+	public static void main(String[] args) {
+		System.out.println("WTF");
+		
+		Result r1 = new Result(99.9);
+		Result r2 = new Result("shit");
+		System.out.println(r1.getDatatype());
+		System.out.println(r1);
+		System.out.println(r2);
+		System.out.println("WTF");
+		
+	}
+	
+}
