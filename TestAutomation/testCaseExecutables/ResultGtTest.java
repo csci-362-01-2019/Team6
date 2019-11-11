7b867a7943c95ee61e7a2a6f973feb8a30584f58
Alan Arsiniega <arsiniegaa@cofc.edu>
2019-11-11 17:25:23 -0500

    Updated to return a null string

--- TestAutomation/testCaseExecutables/ResultGtTest.java
+++ TestAutomation/testCaseExecutables/ResultGtTest.java
@@ -4,13 +4,21 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Date; 
 import org.openmrs.logic.result.Result;
+import org.openmrs.logic.result.EmptyResult;
 
 public class ResultGtTest {
 
 	public static void main(String[] args) {
+		
+		Result emptyResult = new EmptyResult();
+		
 		Result rgt = new Result(Double.parseDouble(args[0]));
 		Integer int1 = Integer.parseInt(args[1]);
-		System.out.println(rgt.gt(int1));
+		
+		if (rgt.gt(int1) instanceof EmptyResult)
+			System.out.println("null");
+		else 
+			System.out.println(rgt.gt(int1));
 	}
 
 }
